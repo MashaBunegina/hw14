c25efbe557f69840ace701bb369e21493a44bf82
Maria <maria.bunegina@mail.ru>
2022-10-10 22:10:42 +0300

    .

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,9 @@ public class Main {
         System.out.println("publishingYear = " + autumn.getPublishingYear());
         Book blizzard = new Book("Метель", 1819);
         autumn.setPublishingYear(1816);
+        System.out.println("blizzard name = " + blizzard.getName());
         System.out.println("autumn.getPublishingYear =" + autumn.getPublishingYear());
+        System.out.println("blizzard.getPublishingYear =" + blizzard.getPublishingYear());
 
     Author author =  new Author("Александр Пушкин");
         System.out.println( "author.NameAuthor = " + author.NameAuthor);
